fb66fc419e80d4a41876baa656b68d56c5df180b
attakei <attakei@gmail.com>
2015-12-09 20:53:57 +0900

    [wip] google_domain only

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -235,9 +235,16 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2,
-    Rails.application.secrets.google_client_id,
-    Rails.application.secrets.google_client_secret
+  if Rails.application.secrets.google_domain
+    config.omniauth :google_oauth2,
+      Rails.application.secrets.google_client_id,
+      Rails.application.secrets.google_client_secret,
+      {:hd => Rails.application.secrets.google_domain}
+  else
+    config.omniauth :google_oauth2,
+      Rails.application.secrets.google_client_id,
+      Rails.application.secrets.google_client_secret
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
